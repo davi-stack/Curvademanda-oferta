c129c68ab0168a7413c408ccf77a997f84adca06
davi-stack <61973935+davi-stack@users.noreply.github.com>
2022-02-28 22:03:49 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,33 @@
+package Classes;
+
+public class Produto {
+ private String nome;
+ private int quantidadeVendida;
+ private int quantidadeDisponivevel;
+ private float preco;
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public int getQuantidadeVendida() {
+	return quantidadeVendida;
+}
+public void setQuantidadeVendida(int quantidadeVendida) {
+	this.quantidadeVendida = quantidadeVendida;
+}
+public int getQuantidadeDisponivevel() {
+	return quantidadeDisponivevel;
+}
+public void setQuantidadeDisponivevel(int quantidadeDisponivevel) {
+	this.quantidadeDisponivevel = quantidadeDisponivevel;
+}
+public float getPreco() {
+	return preco;
+}
+public void setPreco(float preco) {
+	this.preco = preco;
+}
+ 
+}
